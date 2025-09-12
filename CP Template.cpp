d9771453f772ca6480ec8d646f059d0685601682
Md. Mehraj Hossain Akash <mehrajakash81@gmail.com>
2025-09-12 11:58:11 +0600

    shortcut

---
+++ CP Template.cpp
@@ -0,0 +1,36 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vector<int>> vvi;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef double dl;
+
+#define endl '\n'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll num = 50;
+    cout<< "eh"<<endl;
+    cout<<num;
+
+  return 0;
+}
